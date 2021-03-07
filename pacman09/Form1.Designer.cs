57685febd92f957e07e22fd09b3eb92d528cf54f
Ramazan Kabadayı <ramazan_kabadayi_@outlook.com>
2021-03-07 18:59:57 +0300

    Add project files.

---
+++ pacman09/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace pacman09
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxPacman = new System.Windows.Forms.PictureBox();
+            this.PBYem = new System.Windows.Forms.PictureBox();
+            this.labelYem = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPacman)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBYem)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxPacman
+            // 
+            this.pictureBoxPacman.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxPacman.Image")));
+            this.pictureBoxPacman.Location = new System.Drawing.Point(3, 21);
+            this.pictureBoxPacman.Name = "pictureBoxPacman";
+            this.pictureBoxPacman.Size = new System.Drawing.Size(98, 76);
+            this.pictureBoxPacman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPacman.TabIndex = 1;
+            this.pictureBoxPacman.TabStop = false;
+            // 
+            // PBYem
+            // 
+            this.PBYem.Image = global::pacman09.Properties.Resources.yem;
+            this.PBYem.Location = new System.Drawing.Point(481, 72);
+            this.PBYem.Name = "PBYem";
+            this.PBYem.Size = new System.Drawing.Size(24, 25);
+            this.PBYem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PBYem.TabIndex = 2;
+            this.PBYem.TabStop = false;
+            // 
+            // labelYem
+            // 
+            this.labelYem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelYem.Location = new System.Drawing.Point(260, 9);
+            this.labelYem.Name = "labelYem";
+            this.labelYem.Size = new System.Drawing.Size(209, 42);
+            this.labelYem.TabIndex = 3;
+            this.labelYem.Text = "Puan:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.PBYem);
+            this.panel1.Controls.Add(this.pictureBoxPacman);
+            this.panel1.Location = new System.Drawing.Point(55, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(593, 270);
+            this.panel1.TabIndex = 4;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(679, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "İpucu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(777, 425);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelYem);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PACMAN";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPacman)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBYem)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBoxPacman;
+        private System.Windows.Forms.PictureBox PBYem;
+        private System.Windows.Forms.Label labelYem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ pacman09/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pacman09
+{
+    public partial class Form1 : Form
+    {       
+        int yemsayisi = 0;
+        int MouseX;// bu satır mouse'un X değerini global değişken olarak tutar
+        int MouseY;// bu satır mouse'un Y değerini global değişken olarak tutar
+        int zaman = 80;//Pacmanin hedefe varması için kaç milisaniye gerektiği
+        bool kazandimi=false;//oyunun kazanılıp kazanılmadığını tutan değişken
+        float derece = 0;//Pacmanin mousa kaç derecelik açıyla bakmasını tutan global değişken
+
+        public async void Pacmanfonksiyonu()//pacmanın ana fonksiyonu, tanımlanış şekliyle programın işleyişini engellemeden çalışıyor
+        {          
+            while (!kazandimi)// Oyun kazanılana dek çalışacak olan döngü
+            {
+              
+               if(MouseX!=pictureBoxPacman.Location.X || MouseY != pictureBoxPacman.Location.Y)// Pacmanin mause ile aynı konumda olup olmadığını kontrol eder
+                {                   
+                    await Task.Delay(10); //hareketi 10 milisaniye bekletir
+                    
+                    // mousenin ve pacmanin arasındaki mesafeyi verir
+                    int deltax = (MouseX - pictureBoxPacman.Location.X);
+                    int deltay = (MouseY - pictureBoxPacman.Location.Y);
+                    pictureBoxPacman.Location = new Point(pictureBoxPacman.Location.X + (deltax /zaman),pictureBoxPacman.Location.Y + (deltay / zaman));// pacmanin yeni koordinasyon bilgisini, birim zamanda gitmesi gereken yol ile eski koordinasyon bilgisine ekleyerek bulur.
+
+                    Image pacman = Image.FromFile("acik.png");//Pacmanin ağzının açık olduğu resmi alır
+                    
+                    pictureBoxPacman.Image = RotateImage(pacman, derece * 40);// yukarıda aldığı resmi döndürerek pictureboxa koyar                                                            
+                }
+
+                if (pictureBoxPacman.Bounds.IntersectsWith(PBYem.Bounds))// yem ve pacmanin pictureboxlarının kesişip kesişmediklerini denetler
+                {
+                    yemsayisi++;// pacmanın yediği yem sayısını arttırır
+                    Random rastgele = new Random();
+                    int xrastgele = rastgele.Next(0, panel1.Width-10);  // panelin genişliğine göre rastgele bir x değeri üretir
+                    int yrastgele = rastgele.Next(0, panel1.Height-10); // panelin genişliğine göre rastgele bir y değeri üretir
+                    PBYem.Location = new Point(xrastgele, yrastgele);   // yemin yeni konumunu oluşturur
+                }
+                labelYem.Text = "PUAN: "+yemsayisi.ToString();// pacmanin yediği yem sayısını label üzerinde gösterir
+                if (yemsayisi == 15)//Oyuncunun oyunun amacına ulaşıp ulaşmadığını kontrol eder
+                {
+
+                   
+                    DialogResult yeniden = MessageBox.Show("Yeniden Oynamak İster Misiniz?", "Tebrikler!! Kazandınız.", MessageBoxButtons.YesNo);//oyun kazanıldıysa ,oyunun kazanıldığını belirten messageboxu oluştutur ve yeniden oynamk istermisiniz diye sorar
+                    if (yeniden == DialogResult.Yes)//cevap evet ise yenilen yem sayısını sıfırlar ve yeniden başlatır
+                    {
+                        yemsayisi = 0;
+                        kazandimi = false;
+                    }
+                    else if (yeniden == DialogResult.No)
+                    {
+                        kazandimi = true;// oyunun kazanıldığını tutar        
+                        this.Close();//formu kapatır
+                    }
+                               
+                }                            
+            };  
+           
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            Pacmanfonksiyonu();// oyunun ana döngüsü olan fonksiyonu başlatır  
+        }
+
+        public static Image RotateImage(Image img, float rotationAngle)//Verilen derece değerine göre resmi döndürür kaynak: https://stackoverflow.com/questions/2163829/how-do-i-rotate-a-picture-in-winforms
+        {           
+            Bitmap bmp = new Bitmap(img.Width, img.Height);//Boş bir bitmap resmi oluşturur
+          
+            Graphics gfx = Graphics.FromImage(bmp);//Bitmap resmini Graphics objesine dönüştürür
+           
+            gfx.TranslateTransform((float)bmp.Width / 2, (float)bmp.Height / 2);//Döndürme noktasını resmin merkezi olarak ayarlar
+           
+            //resmi döndürür
+            gfx.RotateTransform(rotationAngle);
+            gfx.TranslateTransform(-(float)bmp.Width / 2, -(float)bmp.Height / 2);
+
+            gfx.InterpolationMode = InterpolationMode.HighQualityBicubic; //görüntü kalitesini bozmamak için InterpolationMode u HighQualityBicubic e seçer                     
+
+            gfx.DrawImage(img, new Point(0, 0)); //Yeni resmi grafik objesine çizer
+
+            gfx.Dispose(); //Boşu boşuna ram harcamamak için grafik objesini kapatır
+
+            return bmp;//resmi verir
+        }
+        
+        private void Form1_MouseMove(object sender, MouseEventArgs e) // mouse un formdaki hareketini algılar
+        {           
+            derece = Convert.ToSingle(Math.Atan2(e.Y - pictureBoxPacman.Location.Y, e.X - pictureBoxPacman.Location.X));//dereceyi hesaplayıp float türüne çevirir
+            Image pacman = Image.FromFile("Kapali.png");//pacmanin ağzını kapatır
+            pictureBoxPacman.Image = RotateImage(pacman, derece * 40);//pacmani döndürür                                                                                 
+        }
+      
+        private void panel1_MouseMove(object sender, MouseEventArgs e)// mouse un paneldeki hareketini algılar
+        {
+
+            MouseX = e.X;// mouse konumunu, pacmanin hareket etmesi için global değişkenlere girer
+            MouseY = e.Y;
+            derece = Convert.ToSingle(Math.Atan2(MouseY - pictureBoxPacman.Location.Y, MouseX - pictureBoxPacman.Location.X));//dereceyi hesaplayıp float türüne çevirir
+            Image pacman = Image.FromFile("Kapali.png");//pacmanin ağzını kapatır
+            pictureBoxPacman.Image = RotateImage(pacman, derece * 40);//pacmani döndürür
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)//mouse tuşuna basılı tutulduğunda pacmanin hızını arttırır
+        {           
+                zaman = 10;                             
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)//mouse tuşunun bırakılmasıyla pacmanin hızı eski haline döner 
+        {           
+                zaman = 80;                   
+        }
+
+        private void button1_Click(object sender, EventArgs e)//oyun ile ilgili bir ipucu verir
+        { 
+            MessageBox.Show("Pacman köşelere yaklaştığında Mouse tuşlarına basınız.");
+        }
+    }
+}
+      
